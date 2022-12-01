403adfb614f2e3f2699337a6fdd2c9b9fa3cf584
oreoA09 <oreo.akanni@gmail.com>
2022-12-01 20:39:06 +0000

    Adding elements to navbar component

---
+++ src/components/Navbar.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import './Navbar.css'
+import SearchIcon from '@mui/icons-material/Search';
+import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone';
+
+function Navbar() {
+  return (
+    <div className='navbar_container'>
+        <div>
+        <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/1920px-Netflix_2015_logo.svg.png' alt='netflix_logo' width='100px'/>
+
+        <nav>
+            <a href='#'>Home</a>
+            <a href='#'>Tv Shows</a>
+            <a href='#'>Movies</a>
+            <a href='#'>Latest</a>
+            <a href='#'>My List</a>
+        </nav>
+        </div>
+        
+        <div>
+        <SearchIcon/>
+        <NotificationsNoneIcon/>
+
+        <img src='https://i.pinimg.com/originals/0d/dc/ca/0ddccae723d85a703b798a5e682c23c1.png' alt='avatar' width='20px'/>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
